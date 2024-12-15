82654f67e3bfd50e65c6a636730b99a319447f5f
Gabor Klemm <klemm.gabor@gmail.com>
2024-12-15 22:34:01 +0100

    updated build.sh

--- build/_deps/glm-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kullancs/Documents/Programming/C++/OpenGL/Project1/build/_deps/glm-src
-# Build directory: /home/kullancs/Documents/Programming/C++/OpenGL/Project1/build/_deps/glm-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
