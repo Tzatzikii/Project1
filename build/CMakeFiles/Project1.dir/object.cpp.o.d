a6dd69adac7af4b58b1901d9297255c2d75898ba
Gabor Klemm <klemm.gabor.14@gmail.com>
2024-12-15 20:22:21 +0100

    fixed cmake deps for glm

--- build/CMakeFiles/Project1.dir/object.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Project1.dir/object.cpp.o: \
- /home/kullancs/Documents/Programming/C++/OpenGL/Project1/src/main/object.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/Project1.dir/scene.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/Project1.dir/scene.cpp.o: \
- /home/kullancs/Documents/Programming/C++/OpenGL/Project1/src/main/scene.cpp \
- /usr/include/stdc-predef.h \
- /home/kullancs/Documents/Programming/C++/OpenGL/Project1/src/main/scene.hpp
